3d826221e48949812ad7ee11554ce654b9fdbe32
seisvalt <jl.vargass@gmail.com>
2017-06-12 20:19:55 -0500

    Initial database

---
+++ src/db/database.ts
@@ -0,0 +1,46 @@
+import Dexie from "dexie";
+
+export interface ITransaction {
+  id?: number;
+  amount: number;
+  lat: number;
+  lng: number;
+  imageUrl: string;
+  title: string;
+}
+export class TransactionAppDb extends Dexie {
+  transactions: Dexie.Table<ITransaction, number>;
+
+  constructor() {
+    super("moneymapdb");
+    this.version(1).stores({
+      transactions: '++id, amount, lat, lng, imageUrl, title',
+    });
+
+    this.transactions.mapToClass(Transaction);
+  }
+}
+
+export class Transaction implements ITransaction {
+  id?: number;
+  amount: number;
+  lat: number;
+  lng: number;
+  imageUrl: string;
+  title: string;
+
+  constructor(amount: number, title: string, lat?: number, lng?: number, imageUrl?: string, id?: number) {
+    this.amount = amount;
+    this.title = title;
+    if (lat) this.lat = lat;
+    if (lng) this.lng = lng;
+    if (imageUrl) this.imageUrl = imageUrl;
+    if (lat) this.id = id;
+  }
+
+  save() {
+    return db.transactions.add(this);
+  }
+}
+
+export let db = new TransactionAppDb();

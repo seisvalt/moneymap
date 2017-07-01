2f2c75e2512bd9990ccfc2e7ca9113df537fca20
seisvalt <jl.vargass@gmail.com>
2017-07-01 17:28:59 -0500

    Added transaccion markers

--- src/pages/map/map.ts
+++ src/pages/map/map.ts
@@ -1,7 +1,14 @@
 import {Component} from "@angular/core";
 import {IonicPage, NavController, NavParams} from "ionic-angular";
-import {GoogleMap, LatLng} from "@ionic-native/google-maps";
+import {
+  GoogleMap,
+  GoogleMapsEvent,
+  LatLng,
+  Marker,
+  MarkerOptions
+} from "@ionic-native/google-maps";
 import {GeolocationService} from "../../services/geolocation.service";
+import {ITransaction, Transaction} from "../../db/database";
 
 /**
  * Generated class for the MapPage page.
@@ -15,11 +22,16 @@ import {GeolocationService} from "../../services/geolocation.service";
   templateUrl: 'map.html',
 })
 export class MapPage {
+  map: GoogleMap = null;
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public geolocator: GeolocationService) {
   }
 
-  ionViewDidLoad() {
+  //se carga solo una vez
+  //ionViewDidLoad() {}
+  //se carga cada vez que se llama la vista
+  //the view is load when is call from controller
+  ionViewDidEnter() {
     console.log('ionViewDidLoad MapPage');
     this.geolocator.getLocation().then((result) => {
       this.loadMap(result.coords.latitude, result.coords.longitude);
@@ -28,7 +40,7 @@ export class MapPage {
 
   loadMap(lat, lng) {
     let location: LatLng = new LatLng(lat, lng);
-    new GoogleMap("map", {
+    this.map = new GoogleMap("map", {
       'controls': {
         'compass': true,
         'indoorPicker': true
@@ -37,7 +49,27 @@ export class MapPage {
         'latLng': location,
         'zoom': 15
       }
-    })
+    });
+    this.map.on(GoogleMapsEvent.MAP_READY).subscribe(() => this.loadMarkers());
   }
 
+  loadMarkers() {
+    Transaction.all().then((results) => this.loadTransactionsMarkers(results));
+  }
+
+  private loadTransactionsMarkers(results: Array<ITransaction>) {
+
+    for (var i = 0; i < results.length; ++i) {
+      let transaction = results[i];
+      let markerLocation: LatLng = new LatLng(transaction.lat, transaction.lng);
+      let markerOptions: MarkerOptions = {
+        position: markerLocation,
+        title: transaction.title,
+        icon: "blue"
+      };
+      this.map.addMarker(markerOptions).then((marker: Marker) => {
+        marker.showInfoWindow();
+      }).catch(err => console.log(err));
+    }
+  }
 }
